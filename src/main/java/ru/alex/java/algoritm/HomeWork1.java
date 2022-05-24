af976bc12448ba71a5ad578528e0a65f9583c8bf
Alexander <89163278541@mail.ru>
2022-05-24 22:37:08 +0300

    less1 complete

---
+++ src/main/java/ru/alex/java/algoritm/HomeWork1.java
@@ -0,0 +1,43 @@
+package ru.alex.java.algoritm;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println(exponentiation(8, 3));
+        System.out.println(exponentiationWithParity(8, 4));
+        sumFromZeroToOneHundred();
+    }
+
+    //1. возведение в степень
+    public static long exponentiation(int base, int exponent) {
+        long variable = 1;
+        for (int i = 0; i < exponent; i++) {
+            variable *= base;
+        }
+        return variable;
+    }
+
+    //2. возведение в степень (с использованием свойства чётности степени)
+    public static long exponentiationWithParity(int base, int exponent) {
+        long variable = 1;
+        if (exponent % 2 == 0) {
+            for (int i = 0; i < exponent / 2; i++) {
+                variable *= base;
+            }
+            variable *= variable;
+        } else {
+            for (int i = 0; i < exponent; i++) {
+                variable *= base;
+            }
+        }
+        return variable;
+    }
+
+    //3. получить сумму всех чисел в ряду от 0 до 100.
+    public static void sumFromZeroToOneHundred() {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
